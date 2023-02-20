8f0014e2e12cf7b459cee209492edafbc01ea019
Saana Lapinkangas <118981440+saanalapinkangas@users.noreply.github.com>
2023-02-20 08:48:24 +0200

    Add files via upload

---
+++ laskin/build/moc_mainwindow.cpp
@@ -0,0 +1,188 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.3.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.3.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    const uint offsetsAndSize[36];
+    char stringdata0[252];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs), len 
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 10), // "MainWindow"
+QT_MOC_LITERAL(11, 13), // "nappi1Handler"
+QT_MOC_LITERAL(25, 0), // ""
+QT_MOC_LITERAL(26, 13), // "nappi2Handler"
+QT_MOC_LITERAL(40, 13), // "nappi3Handler"
+QT_MOC_LITERAL(54, 13), // "nappi4Handler"
+QT_MOC_LITERAL(68, 13), // "nappi5Handler"
+QT_MOC_LITERAL(82, 13), // "nappi6Handler"
+QT_MOC_LITERAL(96, 13), // "nappi7Handler"
+QT_MOC_LITERAL(110, 13), // "nappi8Handler"
+QT_MOC_LITERAL(124, 13), // "nappi9Handler"
+QT_MOC_LITERAL(138, 13), // "nappi0Handler"
+QT_MOC_LITERAL(152, 15), // "nappiAddHandler"
+QT_MOC_LITERAL(168, 15), // "nappiSubHandler"
+QT_MOC_LITERAL(184, 15), // "nappiMulHandler"
+QT_MOC_LITERAL(200, 15), // "nappiDivHandler"
+QT_MOC_LITERAL(216, 17), // "nappiClearHandler"
+QT_MOC_LITERAL(234, 17) // "nappiEnterHandler"
+
+    },
+    "MainWindow\0nappi1Handler\0\0nappi2Handler\0"
+    "nappi3Handler\0nappi4Handler\0nappi5Handler\0"
+    "nappi6Handler\0nappi7Handler\0nappi8Handler\0"
+    "nappi9Handler\0nappi0Handler\0nappiAddHandler\0"
+    "nappiSubHandler\0nappiMulHandler\0"
+    "nappiDivHandler\0nappiClearHandler\0"
+    "nappiEnterHandler"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+      10,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      16,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  110,    2, 0x0a,    1 /* Public */,
+       3,    0,  111,    2, 0x0a,    2 /* Public */,
+       4,    0,  112,    2, 0x0a,    3 /* Public */,
+       5,    0,  113,    2, 0x0a,    4 /* Public */,
+       6,    0,  114,    2, 0x0a,    5 /* Public */,
+       7,    0,  115,    2, 0x0a,    6 /* Public */,
+       8,    0,  116,    2, 0x0a,    7 /* Public */,
+       9,    0,  117,    2, 0x0a,    8 /* Public */,
+      10,    0,  118,    2, 0x0a,    9 /* Public */,
+      11,    0,  119,    2, 0x0a,   10 /* Public */,
+      12,    0,  120,    2, 0x0a,   11 /* Public */,
+      13,    0,  121,    2, 0x0a,   12 /* Public */,
+      14,    0,  122,    2, 0x0a,   13 /* Public */,
+      15,    0,  123,    2, 0x0a,   14 /* Public */,
+      16,    0,  124,    2, 0x0a,   15 /* Public */,
+      17,    0,  125,    2, 0x0a,   16 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->nappi1Handler(); break;
+        case 1: _t->nappi2Handler(); break;
+        case 2: _t->nappi3Handler(); break;
+        case 3: _t->nappi4Handler(); break;
+        case 4: _t->nappi5Handler(); break;
+        case 5: _t->nappi6Handler(); break;
+        case 6: _t->nappi7Handler(); break;
+        case 7: _t->nappi8Handler(); break;
+        case 8: _t->nappi9Handler(); break;
+        case 9: _t->nappi0Handler(); break;
+        case 10: _t->nappiAddHandler(); break;
+        case 11: _t->nappiSubHandler(); break;
+        case 12: _t->nappiMulHandler(); break;
+        case 13: _t->nappiDivHandler(); break;
+        case 14: _t->nappiClearHandler(); break;
+        case 15: _t->nappiEnterHandler(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.offsetsAndSize,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_MainWindow_t
+, QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 16)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 16;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 16)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 16;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
